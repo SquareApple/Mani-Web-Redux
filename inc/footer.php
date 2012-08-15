4add64c5a59c11b23eb48d3641eb558ff7a45944
Matt M <mcm@they-got.us>
2012-08-14 20:58:50 -0400

    Fixed a typo in the footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,5 +1,5 @@
  <div class="footer"> 
-  Powered by Mani Web Redux V0.0.4.0: Executed 
+  Powered by Mani Web Redux V0.4.1: Executed 
   <?php
    /* Get the number of SQL Queries and output it */
    $queries = $sql->nQuery;
